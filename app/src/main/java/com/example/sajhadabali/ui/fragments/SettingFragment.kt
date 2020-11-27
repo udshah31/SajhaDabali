5b3975809ea7b1559a8345294aad5af8388481d9
ethan <udshah31@gmail.com>
2020-11-27 20:24:46 +0545

    setting xml is added

--- app/src/main/java/com/example/sajhadabali/ui/fragments/SettingFragment.kt
+++ app/src/main/java/com/example/sajhadabali/ui/fragments/SettingFragment.kt
@@ -1,14 +1,14 @@
 package com.example.sajhadabali.ui.fragments
 
 import android.os.Bundle
-import android.view.View
-import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
 import com.example.sajhadabali.R
 
-class SettingFragment: Fragment(R.layout.fragment_setting){
+class SettingFragment : PreferenceFragmentCompat() {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.setting, rootKey)
     }
+
 
 }
